f049ecb93325e11bc510f2a544543a8c807029b9
Maksym Nekohdiuk <maksym.glosl.net>
2024-02-25 21:21:39 +0200

    add public ip

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "terraform" {
-  ami           = data.aws_ami.ubuntu22.id
-  instance_type = "t2.micro"
-  tags          = var.tags
-  key_name      = var.key_name
+  ami                         = data.aws_ami.ubuntu22.id
+  instance_type               = "t2.micro"
+  tags                        = var.tags
+  key_name                    = var.key_name
+  associate_public_ip_address = true
 }
 
 output "ami" {
