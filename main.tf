d8bff423062d88c7e4f72f58bc85e95615fa5f9a
Maksym Nekohdiuk <maksym.glosl.net>
2024-02-25 14:58:31 +0200

    ec2 data update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "terraform" {
+resource "aws_instance" "terraform" {  
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags          = var.tags
 }
